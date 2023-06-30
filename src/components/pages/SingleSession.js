41e4586d025f484142d087a52192e27d56e83d08
rettusm <rettusm@wwu.edu>
2023-06-30 16:50:34 -0700

    random choice

--- src/components/pages/SingleSession.js
+++ src/components/pages/SingleSession.js
@@ -83,6 +83,10 @@ export default function SingleSession() {
       { restaurant: null, score: 0 }
     );
     setShowPopup(true);
+    if(setTie){
+      setSelectedRestaurant(bestRestaurant[Math.floor(Math.random() * bestRestaurant.length)]);
+      console.log(selectedRestaurant);
+    }
     setSelectedRestaurant(bestRestaurant);
   };
 
@@ -224,50 +228,43 @@ export default function SingleSession() {
           ) : (
             <div>
               <h2>Most Matched Restaurants(Tie):</h2>
-              {restaurants
-                .filter((restaurant) => {
-                  const score =
-                    (desirabilities1[restaurant.id] || 0) +
-                    (desirabilities2[restaurant.id] || 0);
-                  return score === selectedRestaurant.score;
-                })
-                .map((restaurant) => (
-                  <div key={restaurant.id}>
-                    <div className="restaurant-details">
-                      <a href={restaurant.url} target="_blank" rel="noreferrer">
-                        <div className="restaurant-image">
-                          <img
-                            src={restaurant.image_url}
-                            alt={restaurant.name}
-                          />
-                        </div>
-                      </a>
-                      <div className="restaurant-info">
-                        <h3>{restaurant.name}</h3>
-
-                        <div className="restaurant-ratings">
-                          <span className="rating">
-                            Rating: {restaurant.rating}
-                          </span>
-                          <span className="review-count">
-                            ({restaurant.review_count} reviews)
-                          </span>
-                        </div>
-                        <div className="restaurant-links">
-                          <a onClick={handleConfirmChoice}>Confirm Choice</a>
-                          <a
-                            href={`https://www.google.com/maps/search/?api=1&query=${restaurant.coordinates.latitude},${restaurant.coordinates.longitude}`}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            Get Directions
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                    {/* <p>Score: {selectedRestaurant.score}</p> */}
+              <p id = "tie">Your scores resulted in a tie!<br></br> No worries our advanced algorithm selected an excellent choice for you.</p>
+              <div className="restaurant-details">
+                <a
+                  href={selectedRestaurant.restaurant.url}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="restaurant-image">
+                    <img
+                      src={selectedRestaurant.restaurant.image_url}
+                      alt={selectedRestaurant.restaurant.name}
+                    />
                   </div>
-                ))}
+                </a>
+
+                <div className="restaurant-info">
+                  <h3>{selectedRestaurant.restaurant.name}</h3>
+                  <div className="restaurant-ratings">
+                    <span className="rating">
+                      Rating: {selectedRestaurant.restaurant.rating}
+                    </span>
+                    <span className="review-count">
+                      ({selectedRestaurant.restaurant.review_count} reviews)
+                    </span>
+                  </div>
+                  <div className="restaurant-links">
+                    <a onClick={handleConfirmChoice}>Confirm Choice</a>
+                    <a
+                      href={`https://www.google.com/maps/search/?api=1&query=${selectedRestaurant.restaurant.coordinates.latitude},${selectedRestaurant.restaurant.coordinates.longitude}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Get Directions
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
           )}
           <button onClick={() => setSelectedRestaurant(null)}>Close</button>
